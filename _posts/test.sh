14379c46fdd9222dd657500399007213d012f019
TNAStaff <>
2019-11-25 12:39:13 +0100

    Implemented tagging for Years and Games.

--- _posts/test.sh
+++ _posts/test.sh
@@ -1,3 +1,14 @@
-grep -ril "abe's oddysee" $(find . -name '*.html') | while read -r line ; do
-sed -n 's/tags: \[\(.*\)\]/tags: ["abe'\''s oddysee", \1]/p' $line
+# Find all HTML files.
+htmls=$(find . -name '*.html')
+
+# Delete all tags.
+for i in $htmls ; do
+  sed -i 's/tags: \[.*\]/tags: []/' $i
+done
+
+# Add Game tags.
+for i in $'abe\'s oddysee' $'abe\'s exoddus' $'munch\'s oddysee' $'stranger\'s wrath' $'new \'n\' tasty' $'soulstorm' {1997..2019}; do
+  grep -ril "$i" $htmls | while read -r line ; do
+    sed -i "s/tags: \[\(.*\)\]/tags: [\"$i\", \1]/" $line
+  done
 done
